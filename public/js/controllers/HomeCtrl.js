589f6b432826d1f0750f663695c6e8af83c7e72d
Kyle <kyle.l.parker16@gmail.com>
2015-11-05 19:34:05 -0700

    created loop to generate form divs and format to fit phones. exports to computer and phones.

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,6 +1,16 @@
 myApp.controller('homeController', function($scope,CowFactory) {
     
     $scope.cowArray = [{NAME: "MILKY", AGE: 22}, {NAME: "JGC", AGE: 70}];
+    $scope.fieldArray = [{fieldName:"Tag Number", dataType:"number"},
+    					{fieldName:"Birth Day", dataType:"date"},
+    					{fieldName:"Shot Type 1", dataType:"date"},
+    					{fieldName:"Shot Type 2", dataType:"date"},
+    					{fieldName:"Shot Type 3", dataType:"date"},
+    					{fieldName:"Shot Type 4", dataType:"date"},
+    					{fieldName:"weight", dataType:"number"},
+    					{fieldName:"mother", dataType:"text"}
+    					];
+
 
 	var getCows = function(){
 		CowFactory.get()
@@ -14,10 +24,5 @@ myApp.controller('homeController', function($scope,CowFactory) {
 	angular.element(document).ready(function () {
 		getCows();
 	});
-
-	
-
-
-
 
 });
